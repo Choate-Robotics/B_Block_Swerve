7f29c11ddab37966eb789cfef72a14dc65f4dcb7
Ann Song <gmsong1978@gmail.com>
2023-12-01 13:52:15 -0500

    Initial drivetrain

---
+++ config.py
@@ -0,0 +1,26 @@
+# Drivetrain
+front_left_move_id = 1
+front_left_turn_id = 2
+front_left_encoder_port = 1
+front_left_encoder_zeroed_pos = 0
+
+front_right_move_id = 3
+front_right_turn_id = 4
+front_right_encoder_port = 2
+front_right_encoder_zeroed_pos = 0
+
+back_left_move_id = 5
+back_left_turn_id = 6
+back_left_encoder_port = 3
+back_left_encoder_zeroed_pos = 0
+
+back_right_move_id = 7
+back_right_turn_id = 8
+back_right_encoder_port = 4
+back_right_encoder_zeroed_pos = 0
+
+driver_centric: bool = True
+drivetrain_reversed: bool = False
+
+# Gyro
+gyro_id = 0
--- constants.py
+++ constants.py
@@ -1,1 +1,8 @@
-
+# Drivetrain
+drivetrain_turn_gear_ratio = 0
+drivetrain_move_gear_ratio = 0
+drivetrain_move_gear_ratio_as_rotations_per_meter = 0
+track_width = 0
+drivetrain_max_vel = 0
+drivetrain_max_target_accel = 0
+drivetrain_max_angular_vel = 0
