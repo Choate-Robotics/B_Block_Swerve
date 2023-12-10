8cf9b48f12e2ec4d65555cada453eff055b02157
e-bauman <ebauman26@choate.edu>
2023-12-10 12:28:20 -0500

    created 2 commands, one to set angle and one to set speed

--- command/intake.py
+++ command/intake.py
@@ -3,3 +3,38 @@ from subsystem import Intake
 import config
 import math
 
+class SetIntake(SubsystemCommand[Intake]):
+    def __init__(self, subsystem: Intake, angle):
+        super().__init__(subsystem)
+        self.subsystem = subsystem
+        self.angle = angle
+
+    def initialize(self):
+        self.subsystem.set_intake_angle(math.radians(self.angle))
+
+    def execute(self):
+        pass
+
+    def isFinished(self):
+        return self.subsystem.is_at_angle(self.angle)
+    
+    def end(self, interrupted):
+        self.subsystem.set_intake_angle(self.subsystem.get_intake_angle())
+
+class IntakeRoller(SubsystemCommand[Intake]):
+    def __init__(self, subsystem: Intake, speed: float):
+        super().__init__(subsystem)
+        self.subsystem = subsystem
+        self.speed = speed
+
+    def initialize(self):
+        self.subsystem.set_roller_velocity(self.speed)
+
+    def execute(self):
+        pass
+
+    def isFinished(self):
+        return self.subsystem.get_roller_velocity() == self.speed
+
+    def end(self, interrupted):
+        pass
